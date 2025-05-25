59dc49ecd258d232c3b1746201106928dd39605f
itsivag <sivacbrf2@gmail.com>
2025-05-25 20:57:28 +0530

    Modified fastlane config

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "org.itsivag.trackmycard"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.3"
 
     }
     packaging {
